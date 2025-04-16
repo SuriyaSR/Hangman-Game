463119faf3585dc23382c59003f5c030c5386b40
SuriyaSR <103486834+SuriyaSR@users.noreply.github.com>
2025-04-16 21:39:21 +0530

    Update eslint.config.js
    to avoid error for unused var

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,12 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      "no-unused-vars": [
+          "error",
+          {
+              caughtErrors: "none"
+          }
+      ],
     },
   },
 )
